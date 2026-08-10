3baa7c54b1c3bf7b7f43428f406ddb842a4a90c1
imaller43 <b122410306@student.utem.edu.my>
2026-08-10 11:09:51 +0800

    fix: update Firebase config to new project credentials

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,18 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBy0JiVVER3CaZ5R4I1QYSaI4B8z6rZXlE",
-  authDomain: "iotdashboard-667f6.firebaseapp.com",
-  projectId: "iotdashboard-667f6",
-  storageBucket: "iotdashboard-667f6.firebasestorage.app",
-  messagingSenderId: "582199250692",
-  appId: "1:582199250692:web:c6bb42c9bc342d2beb0a51"
+  apiKey: "AIzaSyAFB9FVcOUWx2X5SXyIxQJ_eqKecbSQ7Vs",
+  authDomain: "iotdashboard-45e86.firebaseapp.com",
+  projectId: "iotdashboard-45e86",
+  storageBucket: "iotdashboard-45e86.firebasestorage.app",
+  messagingSenderId: "72552641629",
+  appId: "1:72552641629:web:2f83baa0642db987c749b4"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const messaging = getMessaging(app);
 
-export const VAPID_KEY = "BMnTCGCXNBZd_fFIkymU6CNhK6-tAEKNtpcA2IGJQZ7cR5kjZAJMcQut1iH6Ie_n5dVBJZX_z7NDAsbeUBDrIWg";
+export const VAPID_KEY = "BF8VGx4Aa8QKZ4yUMHgsNMQy4o6M4GRpwZonsgEeLkjOSe4twoW0DwV_uyRfr7GHnzU5Gbi30rYASE2q439V8AQ";
 
 export { app, getToken, onMessage };
